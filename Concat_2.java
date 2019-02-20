b5d7d2e5a43f4bd706037262fe18f6dd2781b029
japneetkaur21 <47344108+japneetkaur21@users.noreply.github.com>
2019-02-20 21:30:59 +0530

    Add files via upload

---
+++ Concat_2.java
@@ -0,0 +1,16 @@
+public class Concat_2 {
+    public static String s1="String1";
+    public static String s2="String2";
+
+    public static String concat()
+    {
+
+        return s1 + s2 ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The concatenated strings are: ");
+        System.out.println(concat());
+    }
+
+}
